d2bf4b6c0e203429ba95dacecfc7496858bb7210
AnshGaikwad <anshyg2002@gmail.com>
2021-11-28 12:10:11 +0530

    modularised create table command

--- src/com/dms/Main.java
+++ src/com/dms/Main.java
@@ -1,5 +1,6 @@
 package com.dms;
 
+import com.dms.commands.Create;
 import com.dms.commands.Help;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -7,6 +8,7 @@ import com.opencsv.CSVWriter;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -17,7 +19,8 @@ public class Main {
 	    while(true){
 	        System.out.print("> ");
 	        String command = sc.nextLine();
-	        System.out.println(command);
+	        System.out.println("Command: " + command);
+            command = command.toUpperCase(Locale.ROOT);
             if(command.contains("HELP")){
                 Help help = new Help();
                 if(command.contains("CREATE TABLE")){
@@ -38,81 +41,10 @@ public class Main {
                     System.out.println("Command doesn't exists");
                 }
             }else if(command.contains("CREATE TABLE")){
-	            String tableName = command.split(" ")[2];
-	            String schemaCSV = "schema.csv";
-                String tableCSV = tableName + ".csv";
-                String attribute = command.substring(command.indexOf("(") + 1);
-                attribute = attribute.substring(0, attribute.indexOf(")"));
-                String[] attributes = attribute.split(" ");
-
-                System.out.println(tableName + Arrays.toString(attributes));
-
-                boolean tableExists = false;
-                CSVWriter tableWriter, schemaWriter;
-
-                File schemaFile = new File(schemaCSV);
-
-                // Checking if the specified file exists or not
-                if (schemaFile.exists()) {
-                    CSVReader reader;
-                    try {
-                        reader = new CSVReader(new FileReader(schemaCSV));
-//                        System.out.println("Reader : "+Arrays.toString(reader.readNext()));
-                        //Read CSV line by line and use the string array as you want
-                        String[] nextLine;
-                        while ((nextLine = reader.readNext()) != null) {
-                            System.out.println("Next Line : "+Arrays.toString(nextLine));
-                            if(nextLine[0].equals(tableName)){
-                                System.out.println("[!!] Table exists already");
-                                tableExists = true;
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else{
-                    try {
-                        File file = new File(schemaCSV);
-                        boolean result = file.createNewFile();
-                        System.out.println("File: " + file);
-                        if(result){
-                            System.out.println("File Created");
-                        }else{
-                            System.out.println("Error");
-                        }
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if(tableExists){
-                    continue;
-                }
-
-                try {
-                    tableWriter = new CSVWriter(new FileWriter(tableCSV));
-                    schemaWriter = new CSVWriter(new FileWriter(schemaCSV, true));
-                    StringBuilder schemaRecord = new StringBuilder((tableName + ","));
-                    StringBuilder tableRecord = new StringBuilder();
-                    for(int i = 0; i < attributes.length; i++){
-                        schemaRecord.append(attributes[i]);
-                        if(i%2 == 0){
-                            tableRecord.append(attributes[i]).append(",");
-                            schemaRecord.append(",");
-                        }
-                    }
-                    String[] schemaRecords = schemaRecord.toString().split(",");
-                    System.out.println(Arrays.toString(schemaRecords));
-                    String[] tableRecords = tableRecord.toString().split(",");
-                    System.out.println(Arrays.toString(tableRecords));
-                    schemaWriter.writeNext(schemaRecords);
-                    schemaWriter.close();
-                    tableWriter.writeNext(tableRecords);
-                    tableWriter.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Create create = new Create(command);
+	            String createdTable = create.createTable();
+                if(createdTable != null)
+                    System.out.println(">> " + createdTable + " Table Created Successfully");
             }
 	        else if(command.contains("DROP TABLE")){
                 String schemaCSV = "schema.csv";
--- src/com/dms/commands/Create.java
+++ src/com/dms/commands/Create.java
@@ -1,4 +1,101 @@
 package com.dms.commands;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Create {
+
+    String command;
+    public Create (String c){
+        command = c;
+    }
+
+    private boolean checkIfTableExists(String schemaCSV, String tableName){
+        File schemaFile = new File(schemaCSV);
+
+        // Checking if the specified file exists or not
+        if (schemaFile.exists()) {
+            CSVReader reader;
+            try {
+                reader = new CSVReader(new FileReader(schemaCSV));
+
+                //Read CSV line by line and use the string array as you want
+                String[] nextLine;
+                while ((nextLine = reader.readNext()) != null) {
+                    if(nextLine[0].equals(tableName)){
+                        return true;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                File file = new File(schemaCSV);
+                boolean result = file.createNewFile();
+                if(!result){
+                    System.out.println("[!!] Error creating new File");
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+
+    private void appendAttributes(String schemaCSV, String tableName){
+
+        CSVWriter tableWriter, schemaWriter;
+
+        String tableCSV = tableName + ".csv";
+        String attribute = command.substring(command.indexOf("(") + 1);
+        attribute = attribute.substring(0, attribute.indexOf(")"));
+        String[] attributes = attribute.split(" ");
+
+        try {
+            tableWriter = new CSVWriter(new FileWriter(tableCSV));
+            schemaWriter = new CSVWriter(new FileWriter(schemaCSV, true));
+            StringBuilder schemaRecord = new StringBuilder((tableName + ","));
+            StringBuilder tableRecord = new StringBuilder();
+            for(int i = 0; i < attributes.length; i++){
+                schemaRecord.append(attributes[i]);
+                if(i%2 == 0){
+                    tableRecord.append(attributes[i]).append(",");
+                    schemaRecord.append(",");
+                }
+            }
+
+            String[] schemaRecords = schemaRecord.toString().split(",");
+            String[] tableRecords = tableRecord.toString().split(",");
+            schemaWriter.writeNext(schemaRecords);
+            schemaWriter.close();
+            tableWriter.writeNext(tableRecords);
+            tableWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String createTable(){
+        String tableName = command.split(" ")[2];
+        String schemaCSV = "schema.csv";
+
+        boolean tableExists = checkIfTableExists(schemaCSV, tableName);
+
+        if(tableExists){
+            System.out.println("[!!] Table exists already");
+            return null;
+        }
+
+        appendAttributes(schemaCSV, tableName);
+
+        return tableName;
+    }
 }
