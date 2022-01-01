1a77c1e06fd7473aeddfb3ec44ff7b13bb5cd4e7
Ansh Gaikwad <anshyg2002@gmail.com>
2022-01-01 22:04:55 +0530

    Logical Operators implemented for Delete

--- src/com/dms/commands/Delete.java
+++ src/com/dms/commands/Delete.java
@@ -66,52 +66,132 @@ public class Delete {
 
     private ArrayList<Integer> getRowsAffected(String tableCSV, String[] condition) {
 
-        String column = condition[0], operator = condition[1], value = condition[2];
-        int index = -1;
         ArrayList<Integer> rowsAffected = new ArrayList<>();
 
-        try {
-            CSVReader reader = new CSVReader(new FileReader(tableCSV));
+        if(condition.length > 3){
+            String logicalOperator = condition[0], column1 = condition[1], operator1 = condition[2], value1 = condition[3],
+            column2 = condition[4], operator2 = condition[5], value2 = condition[6];
 
+            String[] column = {column1, column2};
+            String[] operator = {operator1, operator2};
+            String[] value = {value1, value2};
 
-            if(!command.contains("WHERE")){
-                String[] nextLine = reader.readNext();
-                for (int i = 1; (nextLine = reader.readNext()) != null; i++){
-                    rowsAffected.add(i);
+            ArrayList<ArrayList<Integer>> rowsCanBeAffected =  new ArrayList<>();
+            for(int l = 0; l < 2; l++){
+
+                int index = -1;
+
+                try {
+                    CSVReader reader = new CSVReader(new FileReader(tableCSV));
+
+                    if(!command.contains("WHERE")){
+                        String[] nextLine = reader.readNext();
+                        for (int i = 1; (nextLine = reader.readNext()) != null; i++){
+                            rowsAffected.add(i);
+                        }
+                        return rowsAffected;
+                    }
+
+                    String[] attributes = reader.readNext();
+
+                    for(int i = 0; i < attributes.length; i++){
+                        if(attributes[i].equals(column[l])){
+                            index = i;
+                        }
+                    }
+
+                    int conditionVal = -1;
+                    try{
+                        conditionVal = Integer.parseInt(value[l]);
+                    }catch (NumberFormatException ignored){
+
+                    }
+
+                    OperatorUtil operatorUtil = new OperatorUtil(reader, index, conditionVal, value[l]);
+                    switch (operator[l]) {
+                        case "<=" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.lessThanEqualTo()));
+                        case ">=" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.moreThanEqualTo()));
+                        case "!=" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.notEqualTo()));
+                        case "=" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.equalTo()));
+                        case "<" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.lessThan()));
+                        case ">" -> rowsCanBeAffected.add(new ArrayList<>(operatorUtil.moreThan()));
+                        default -> System.out.println("Easter Egg here");
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                return rowsAffected;
+
+
             }
 
-            String[] attributes = reader.readNext();
+            ArrayList<Integer> rowsAffected1 = new ArrayList<>(rowsCanBeAffected.get(0));
+            ArrayList<Integer> rowsAffected2 = new ArrayList<>(rowsCanBeAffected.get(1));
+            System.out.println(rowsAffected1 + " " + rowsAffected2);
+            if(Objects.equals(logicalOperator, "AND")){
+                rowsAffected1.retainAll(rowsAffected2);
+                return rowsAffected1;
+            }
+            else if(Objects.equals(logicalOperator, "OR")){
+                List<Integer> rowsAffected2Copy = new ArrayList<>(rowsAffected2);
+                rowsAffected2Copy.removeAll(rowsAffected1);
+                rowsAffected1.addAll(rowsAffected2Copy);
+                return rowsAffected1;
+            }else{
+                return null;
+            }
 
-            for(int i = 0; i < attributes.length; i++){
-                if(attributes[i].equals(column)){
-                    index = i;
+
+        }else{
+            String column = condition[0], operator = condition[1], value = condition[2];
+
+
+
+            int index = -1;
+
+            try {
+                CSVReader reader = new CSVReader(new FileReader(tableCSV));
+
+                if(!command.contains("WHERE")){
+                    String[] nextLine = reader.readNext();
+                    for (int i = 1; (nextLine = reader.readNext()) != null; i++){
+                        rowsAffected.add(i);
+                    }
+                    return rowsAffected;
                 }
+
+                String[] attributes = reader.readNext();
+
+                for(int i = 0; i < attributes.length; i++){
+                    if(attributes[i].equals(column)){
+                        index = i;
+                    }
+                }
+
+                int conditionVal = -1;
+                try{
+                    conditionVal = Integer.parseInt(value);
+                }catch (NumberFormatException ignored){
+
+                }
+
+                OperatorUtil operatorUtil = new OperatorUtil(reader, index, conditionVal, value);
+                switch (operator) {
+                    case "<=" -> rowsAffected = operatorUtil.lessThanEqualTo();
+                    case ">=" -> rowsAffected = operatorUtil.moreThanEqualTo();
+                    case "!=" -> rowsAffected = operatorUtil.notEqualTo();
+                    case "=" -> rowsAffected = operatorUtil.equalTo();
+                    case "<" -> rowsAffected = operatorUtil.lessThan();
+                    case ">" -> rowsAffected = operatorUtil.moreThan();
+                    default -> System.out.println("Easter Egg here");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            int conditionVal = -1;
-            try{
-                conditionVal = Integer.parseInt(value);
-            }catch (NumberFormatException ignored){
-
-            }
-
-            OperatorUtil operatorUtil = new OperatorUtil(reader, index, conditionVal, value);
-            switch (operator) {
-                case "<=" -> rowsAffected = operatorUtil.lessThanEqualTo();
-                case ">=" -> rowsAffected = operatorUtil.moreThanEqualTo();
-                case "!=" -> rowsAffected = operatorUtil.notEqualTo();
-                case "=" -> rowsAffected = operatorUtil.equalTo();
-                case "<" -> rowsAffected = operatorUtil.lessThan();
-                case ">" -> rowsAffected = operatorUtil.moreThan();
-                default -> System.out.println("Easter Egg here");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            return rowsAffected;
         }
 
-        return rowsAffected;
     }
 
     private String getCondition(String[] tableAttributes) {
@@ -122,15 +202,21 @@ public class Delete {
         if(condition.contains("AND") || condition.contains("OR")){
 
             StringBuilder sb = new StringBuilder();
+            String conditionsBuilder;
 
             if(condition.contains("AND")){
                 sb.append("AND").append("-");
+                conditionsBuilder = command.substring(command.indexOf("WHERE") + 6, command.indexOf("AND") - 1) + "-" +
+                        command.substring(command.indexOf("AND") + 4);
             }else if(condition.contains("OR")){
                 sb.append("OR").append("-");
+                conditionsBuilder = command.substring(command.indexOf("WHERE") + 6, command.indexOf("OR") - 1) + "-" +
+                        command.substring(command.indexOf("OR") + 3);
+            }else {
+                conditionsBuilder = "";
             }
 
-            String conditionsBuilder = command.substring(command.indexOf("WHERE") + 6, command.indexOf("AND") - 1) + "-" +
-                    command.substring(command.indexOf("AND") + 4);
+
             String[] conditions = conditionsBuilder.split("-");
 
             for(int i = 0; i < 2; i++){
